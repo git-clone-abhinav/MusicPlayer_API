ca993e39e30d3dad0e02c6296c61b3e5a2ba60aa
RaspberryPi <abhi.anu003@gmail.com>
2021-09-13 00:46:42 +0530

    Addding song to queue via API

---
+++ queueGen.py
@@ -0,0 +1,27 @@
+import music
+
+def checkQueue():
+    with open ("/home/pi/Desktop/MusicPlayer_API/queue.txt","r") as f:
+        songs = f.readlines()
+        songList = []
+        for song in songs:
+            songData = song.rstrip("\n").split(", ")
+            print(songData)
+            songList.append(songData)
+        
+
+def updateQueue(songData):
+    with open('/home/pi/Desktop/MusicPlayer_API/queue.txt', 'a') as f:
+        f.write(f"{songData}\n".format())
+        f.close()
+
+def make_songData(name):
+    data = music.get_metadata(name)
+    songData = (data['title'], data['thumbnail'], data['URL'], data['link'])
+    fString = ", ".join(songData)
+    return fString
+
+def addSong(name):
+    updateQueue(make_songData(name))
+
+checkQueue()
